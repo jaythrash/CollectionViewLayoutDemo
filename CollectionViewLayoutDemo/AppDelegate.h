8c53a87e7b6daf11c34aa50625a1f73c32c1afcd
Jay Thrash <jay.thrash@pobox.com>
2013-09-13 10:10:55 -0400

    Initial Commit

---
+++ CollectionViewLayoutDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewLayoutDemo
+//
+//  Created by Jay Thrash on 9/13/13.
+//  Copyright (c) 2013 AirDrop Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewLayoutDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewLayoutDemo
+//
+//  Created by Jay Thrash on 9/13/13.
+//  Copyright (c) 2013 AirDrop Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
